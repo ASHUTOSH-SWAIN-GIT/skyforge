bc2c5a134d73cc8b6448d628c5b707e6975656c2
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-11-23 10:28:15 +0530

    frontend integration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          // Any request to /api/* in the browser...
+          source: "/api/:path*",
+          // ...is securely forwarded to your Go backend
+          destination: "http://localhost:8080/:path*", 
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
---
+++ apps/web/types/index.ts
@@ -0,0 +1,8 @@
+export interface user {
+    id : string;
+    email: string;
+    name:string;
+    avatar_url?:string;
+    provider:string;
+    created_at:string
+}
